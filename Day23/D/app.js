fa818328baba204176907f46b233cf57d8440987
William Choi <ywchoiab@connect.ust.hk>
2018-11-02 12:47:24 +0800

    update works

---
+++ Day23/D/app.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+app.use(bodyParser.json())
+
+var arr = [1,2,3,5]
+
+let sum = arr.reduce(function(a,n){
+    return a + n
+},0);
+
+app.get('/',(req,res)=>{
+	res.send('hello');
+});
+
+app.post('/result', function(req,res){
+    arr = req.body
+    console.log(req.body);
+    console.log(res.json({sum: sum}));
+});
+
+app.listen(8080);
+
